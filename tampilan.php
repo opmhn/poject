b2bf0a68c91e209a19e44a5045f025c51cfac36c
Raihan10M <110074814+Raihan10M@users.noreply.github.com>
2022-07-29 09:11:16 +0700

    Add files via upload

---
+++ tampilan.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+<body style="align-item">
+   <table class="table table-success table-striped " style="width:100px; text-align:center;">
+      <tr>
+         <th>NO</th>
+         <th>NAMA</th>
+         <th>EMAIL</th>
+         <th>PASSWORD</th>
+         <th>JENIS KELAMIN</th>
+         <th>ALAMAT</th>
+         <th>LEVEL</th>
+         <th>NO HP</th>
+         <th>Edit</th>
+         <th>Hapus</th>
+      </tr>
+      <?php
+         include "konek.php";
+         $no = 1;
+         $query = mysqli_query($koneksi, "SELECT * FROM karyawan");
+         while ($data = mysqli_fetch_array($query)) {
+            ?>
+               <tr>
+                  <td><?=$no++?></td>
+                  <td><?=$data['nama']?></td>
+                  <td><?=$data['email']?></td>
+                  <td><?=$data['password']?></td>
+                  <td><?= ($data['jk'] =='l')?'Laki-Laki' : 'Perempuan' ?></td>
+                  <td><?=$data['alamat']?></td>
+                  <td><?=$data['level']?></td>
+                  <td><?=$data['no_hp']?></td>
+                  <td><a href="editkaryawan.php?id=<?= $data['id']?>"><img src="iconE.png" alt="image" height="25" widht="25"></a> </td>
+                  <td><a href="hapus.php?id=<?= $data['id']?>"><img src="iconH.png" alt="image" height="25" widht="25"></a></td>
+               </tr>
+            <?php
+         }
+      ?>
+   </table>
+   <a href="inputkaryawan.php">tambah data</a>
+<script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
