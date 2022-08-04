81fda0d4f81e42a8b6cbdda9f2e7dc75aa50f871
opmhn <110076980+opmhn@users.noreply.github.com>
2022-08-04 09:39:32 +0700

    Add files via upload

---
+++ perulangan.php
@@ -0,0 +1,28 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1252"></head><body><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+<br>
+<b>Notice</b>:  Undefined variable: a in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+<br>
+<b>Notice</b>:  Undefined variable: a in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>5</b><br>
+<br>
+<b>Notice</b>:  Undefined variable: a in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+1<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+2<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+3<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+4<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+5<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+6<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+7<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+8<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+9<br><br>
+<b>Notice</b>:  Undefined variable: la in <b>C:\xampp\htdocs\php\perulangan.php</b> on line <b>4</b><br>
+10<br></body></html>
